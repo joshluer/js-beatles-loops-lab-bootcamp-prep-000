9689ab17faf2b478d76ad3cf7046faf0f1a607e3
Josh Luerssen <joshluer@gmail.com>
2020-05-19 13:36:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ return newFacts;
 
 function iLoveTheBeatles(num) {
   var arr = [];
-  let i = 7; 
+  let i = 0; 
     
     do {arr.unshift("I love the Beatles!") + i;
       i++;
     }
-    while (i < 17);
+    while (i < 15);
       
   return arr;
 }
